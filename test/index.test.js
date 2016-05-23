8a755b862c60d460790c8ef3b56a4d6d6f9b6945
Trevor Rundell <trevor.rundell@gmail.com>
2016-05-23 19:22:17 -0400

    update references to window.driftt -> window.drift

--- test/index.test.js
+++ test/index.test.js
@@ -39,10 +39,10 @@ describe('Drift', function() {
     });
 
     describe('#initialize', function() {
-      it('should create the window.driftt object', function() {
-        analytics.assert(!window.driftt);
+      it('should create the window.drift object', function() {
+        analytics.assert(!window.drift);
         analytics.initialize();
-        analytics.assert(window.driftt);
+        analytics.assert(window.drift);
       });
 
       it('should call #load', function() {
@@ -66,44 +66,66 @@ describe('Drift', function() {
 
     describe('#identify', function() {
       beforeEach(function() {
-        analytics.stub(window.driftt, 'identify');
+        analytics.stub(window.drift, 'identify');
       });
 
       it('should send an id without an email', function() {
         analytics.identify('id');
-        analytics.called(window.driftt.identify, 'id');
+        analytics.called(window.drift.identify, 'id');
       });
 
       it('should send an id with an email', function() {
         analytics.identify('id', { email: 'blackwidow@shield.gov' });
-        analytics.called(window.driftt.identify, 'id', { email: 'blackwidow@shield.gov' });
+        analytics.called(window.drift.identify, 'id', { email: 'blackwidow@shield.gov' });
       });
 
       it('should send an id and traits', function() {
         analytics.identify('id', { email: 'blackwidow@shield.gov' });
-        analytics.called(window.driftt.identify, 'id', { email: 'blackwidow@shield.gov' });
+        analytics.called(window.drift.identify, 'id', { email: 'blackwidow@shield.gov' });
       });
     });
 
     describe('#track', function() {
       beforeEach(function() {
-        analytics.stub(window.driftt, 'track');
+        analytics.stub(window.drift, 'track');
       });
 
       it('should send an event', function() {
         analytics.track('event');
-        analytics.called(window.driftt.track, 'event');
+        analytics.called(window.drift.track, 'event');
       });
 
       it('should send an event and properties', function() {
         analytics.track('event', { property: true });
-        analytics.called(window.driftt.track, 'event', { property: true });
+        analytics.called(window.drift.track, 'event', { property: true });
       });
 
       it('should convert dates to unix timestamps', function() {
         var date = new Date();
         analytics.track('event', { date: date });
-        analytics.called(window.driftt.track, 'event', { date: Math.floor(date / 1000) });
+        analytics.called(window.drift.track, 'event', { date: Math.floor(date / 1000) });
+      });
+    });
+
+    describe('#page', function() {
+      beforeEach(function() {
+        analytics.stub(window.drift, 'track');
+      });
+
+      it('should send an event', function() {
+        analytics.track('event');
+        analytics.called(window.drift.track, 'event');
+      });
+
+      it('should send an event and properties', function() {
+        analytics.track('event', { property: true });
+        analytics.called(window.drift.track, 'event', { property: true });
+      });
+
+      it('should convert dates to unix timestamps', function() {
+        var date = new Date();
+        analytics.track('event', { date: date });
+        analytics.called(window.drift.track, 'event', { date: Math.floor(date / 1000) });
       });
     });
   });
